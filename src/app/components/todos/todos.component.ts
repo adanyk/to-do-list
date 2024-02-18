b15a8b1bcd07be5cc6ea02481e3380f44dc4922c
Adam Nykiel <adamigo10000@wp.pl>
2024-02-18 19:08:04 +0100

    tutorial completed

---
+++ src/app/components/todos/todos.component.ts
@@ -0,0 +1,41 @@
+import { Component, input } from '@angular/core';
+import { Todo } from '../../models/todo';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-todos',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './todos.component.html',
+  styleUrl: './todos.component.css'
+})
+export class TodosComponent {
+  todos: Todo[] = [
+    {
+      content: 'First todo',
+      completed: false
+    },
+    {
+      content: 'Second todo',
+      completed: true
+    }
+  ];
+  inputTodo: string = "";
+
+  toggleDone(id: number): void {
+    this.todos[id].completed = !this.todos[id].completed;
+  }
+
+  deleteTodo(id: number): void {
+    this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo(): void {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    })
+    this.inputTodo = "";
+  }
+}
